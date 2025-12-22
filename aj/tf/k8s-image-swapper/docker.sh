5525dd6df56c2c728a3896a2f216b24ebbd07afa
Veerendra Tanneru <veerendratanneru@gmail.com>
2025-12-22 10:07:37 +0530

    Create docker.sh

---
+++ aj/tf/k8s-image-swapper/docker.sh
@@ -0,0 +1,57 @@
+docker tag v-gha.artifactory.aws.venmo.biz/docker:29.1-dind \
+787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:29.1-dind
+
+docker push 787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:29.1-dind
+
+docker tag v-gha.artifactory.aws.venmo.biz/v-actions-runner:v2.330.0.1 \
+787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:v2.330.0.1
+
+docker push 787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:v2.330.0.1
+
+docker tag v-gha.artifactory.aws.venmo.biz/v-gha-husky:695685b \
+787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:695685b
+
+docker push 787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:695685b
+
+docker tag v-gha.artifactory.aws.venmo.biz/gha-runner-scale-set-controller:v0.12.1.1 \
+787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:v0.12.1.1
+
+docker push 787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:v0.12.1.1
+
+docker tag v-gha.artifactory.aws.venmo.biz/jetstack/cert-manager-controller:v1.4.0 \
+787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:cert-manager-controller-v1.4.0
+
+docker push 787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:cert-manager-controller-v1.4.0
+
+docker tag v-gha.artifactory.aws.venmo.biz/jetstack/cert-manager-cainjector:v1.4.0 \
+787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:cert-manager-cainjector-v1.4.0
+
+docker push 787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:cert-manager-cainjector-v1.4.0
+
+docker tag v-gha.artifactory.aws.venmo.biz/jetstack/cert-manager-webhook:v1.4.0 \
+787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:cert-manager-webhook-v1.4.0
+
+docker push 787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:cert-manager-webhook-v1.4.0
+
+docker tag v-gha.artifactory.aws.venmo.biz/datadoghq/agent:7.41.1 \
+787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:datadog-agent-7.41.1
+
+docker push 787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:datadog-agent-7.41.1
+
+docker tag v-gha.artifactory.aws.venmo.biz/datadoghq/cluster-agent:7.41.1 \
+787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:datadog-cluster-agent-7.41.1
+
+docker push 787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:datadog-cluster-agent-7.41.1
+
+docker tag v-gha.artifactory.aws.venmo.biz/external-secrets:v0.7.1 \
+787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:external-secrets-v0.7.1
+
+docker push 787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:external-secrets-v0.7.1
+
+docker tag v-gha.artifactory.aws.venmo.biz/metrics-server:0.7.1-debian-12-r8 \
+787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:metrics-server-0.7.1-debian-12-r8
+
+docker push 787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:metrics-server-0.7.1-debian-12-r8
+
+docker tag venmo-crowdstrike-docker-rel-remote.artifactory.aws.venmo.biz/falcon-sensor@sha256:8f50960bddd959b2c24ea2a13264db8eba9ea60a45216866b38d66d8e7876f97 \
+787388907485.dkr.ecr.us-east-1.amazonaws.com/sre/prod/docker/gha/node:falcon-sensor
